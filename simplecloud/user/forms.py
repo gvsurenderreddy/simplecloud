d578911c65575967b33bb4612b2b53938729c138
Lei Shi <sjtushi@gmail.com>
2013-07-15 22:13:05 +0800

    Fix part of add virtual machine feature in user page

--- simplecloud/user/forms.py
+++ simplecloud/user/forms.py
@@ -16,12 +16,7 @@ class AddVMForm(Form):
     next = HiddenField()
     name = TextField(u'Choose virtualmachine name', [Required(), Length(VMNAME_LEN_MIN, VMNAME_LEN_MAX)],
             description=u"virtualmachine name.")
-    email = EmailField(u'Email', [Required(), Email()],
-            description=u"What's your email address?")
-    password = PasswordField(u'Password', [Required(), Length(PASSWORD_LEN_MIN, PASSWORD_LEN_MAX)],
-            description=u'%s characters or more! Be tricky.' % PASSWORD_LEN_MIN)
-    name = TextField(u'Choose your username', [Required(), Length(USERNAME_LEN_MIN, USERNAME_LEN_MAX)],
-            description=u"Don't worry. you can change it later.")
+    
     submit = SubmitField('Save')
 
     def validate_name(self, field):
