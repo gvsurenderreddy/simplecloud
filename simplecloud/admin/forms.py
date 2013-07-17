e4d6164a757218ac50cc54f9d128cb4cbac0194b
Lei Shi <sjtushi@gmail.com>
2013-07-17 22:45:49 +0800

    try the translation in python code

--- simplecloud/admin/forms.py
+++ simplecloud/admin/forms.py
@@ -7,6 +7,8 @@ from flask.ext.wtf import Form, ValidationError
 from flask.ext.wtf import Required, Length, EqualTo, Email, NumberRange
 from flask.ext.wtf.html5 import EmailField, IntegerField
 
+from flaskext.babel import gettext as _
+
 from ..user import USER_ROLE, USER_STATUS
 from ..user import User
 from ..utils import (PASSWORD_LEN_MIN, PASSWORD_LEN_MAX,
@@ -16,35 +18,35 @@ from ..utils import (PASSWORD_LEN_MIN, PASSWORD_LEN_MAX,
 class AddUserForm(Form):
     next = HiddenField()
     email = EmailField(u'Email', [Required(), Email()],
-            description=u"What's your email address?")
-    password = PasswordField(u'Password', [Required(), Length(PASSWORD_LEN_MIN, PASSWORD_LEN_MAX)],
-            description=u'%s characters or more! Be tricky.' % PASSWORD_LEN_MIN)
-    name = TextField(u'Choose your username', [Required(), Length(NAME_LEN_MIN, NAME_LEN_MAX)],
-            description=u"Don't worry. you can change it later.")
-    submit = SubmitField(u'Save')
+            description = _(u"What's your email address?"))
+    password = PasswordField(_(u'Password'), [Required(), Length(PASSWORD_LEN_MIN, PASSWORD_LEN_MAX)],
+            description=_(u'%s characters or more! Be tricky.' % PASSWORD_LEN_MIN))
+    name = TextField(_(u'Choose your username'), [Required(), Length(NAME_LEN_MIN, NAME_LEN_MAX)],
+            description=_(u"Don't worry. you can change it later."))
+    submit = SubmitField(_(u'Save'))
 
     def validate_name(self, field):
         if User.query.filter_by(name=field.data).first() is not None:
-            raise ValidationError(u'This username is taken')
+            raise ValidationError(_(u'This username is taken'))
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first() is not None:
-            raise ValidationError(u'This email is taken')
+            raise ValidationError(_(u'This email is taken'))
 
 class EditUserForm(Form):
     next = HiddenField()
-    role_code = RadioField(u"Role", [AnyOf([str(val) for val in USER_ROLE.keys()])],
+    role_code = RadioField(_(u"Role"), [AnyOf([str(val) for val in USER_ROLE.keys()])],
             choices=[(str(val), label) for val, label in USER_ROLE.items()])
-    status_code = RadioField(u"Status", [AnyOf([str(val) for val in USER_STATUS.keys()])],
+    status_code = RadioField(_(u"Status"), [AnyOf([str(val) for val in USER_STATUS.keys()])],
             choices=[(str(val), label) for val, label in USER_STATUS.items()])
     # A demo of datepicker.
-    vm_quota = IntegerField(u"VM Quota", [Required(), NumberRange(VM_QUOTA_MIN, VM_QUOTA_MAX)])
-    created_time = DateField(u'Created time')
-    submit = SubmitField(u'Save')
+    vm_quota = IntegerField(_(u"VM Quota"), [Required(), NumberRange(VM_QUOTA_MIN, VM_QUOTA_MAX)])
+    created_time = DateField(_(u'Created time'))
+    submit = SubmitField(_(u'Save'))
     
     def validate_name(self, field):
         if User.query.filter_by(name=field.data).first() is not None:
-            raise ValidationError(u'This username is taken')
+            raise ValidationError(_(u'This username is taken'))
 
 
 
